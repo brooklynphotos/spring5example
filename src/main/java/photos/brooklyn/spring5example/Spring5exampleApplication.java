35979607b16e5cfabff655061e18f9a02eaae8b3
Brooklyn Photos <guoneng.list@gmail.com>
2019-12-23 20:12:28 +0100

    Initial files

---
+++ src/main/java/photos/brooklyn/spring5example/Spring5exampleApplication.java
@@ -0,0 +1,13 @@
+package photos.brooklyn.spring5example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5exampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5exampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/photos/brooklyn/spring5example/Spring5exampleApplicationTests.java
@@ -0,0 +1,13 @@
+package photos.brooklyn.spring5example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5exampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
